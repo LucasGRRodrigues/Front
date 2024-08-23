1489337822fd6e79247d8af71d3d598a0d35d94a
unknown <2424290040@ACADEMICO.IESB.BR>
2024-08-22 21:54:48 -0300

    Subindo aula 02

--- aulas/aulas01/aula02/index.js
+++ aulas/aulas01/aula02/index.js
@@ -1,6 +1,6 @@
 const root = document.getElementById("root");
 
-// COMPONENTE
+/* componentes */
 function Cabecalho() {
     const header = document.createElement("header");
     return header;
@@ -11,24 +11,137 @@ function Conteudo() {
     return main;
 }
 
-function Rodape() {
+function Rodape(texto) {
     const footer = document.createElement("footer");
+    footer.innerText = `<p>${texto}<p>`;
     return footer;
 }
 
-// PAGINAS
+function Imagem(origem, texto) {
+    const img = document.createElement("img");
+    img.setAttribute("src", origem);
+    img.setAttribute("alt", texto);
+    return img;
+}
+
+function Titulo(texto) {
+    const h1 = document.createElement("h1");
+    h1.innerText = texto;
+    return h1;
+}
+
+function Input(id, tipo, rotulo) {
+   const label = document.createElement("label");
+   label.innertexto = rotulo;
+   label.setAttribute("for", id);
+   const input = document.createElement("input");
+   input.setAttribute("id", id);
+   input.setAttribute("name", id);
+   input.setAttribute("type", tipo);
+   const grupo = document.createElement("div");
+   grupo.append(label, input);
+   return grupo;
+}
+
+function Botao(texto) {
+    const button = document.createElement("input");
+    button.setAttribute("type", "submit");
+    button.setAttribute("value", texto);
+    return button; 
+}
+
+function Link(texto, rota) {
+    const a = document.createElement("a");
+    a.setAttribute("href", rota);
+    a.innerText = texto;
+    return a;
+ }
+
+ function Menu () {
+    const opcoes = [
+        {menu: "Home", rota: "/home"},
+        {menu: "Perfil", rota: "/perfil"},
+        {menu: "Sair", rota: "/login"}
+    ];
+    
+    const lista = document.createElement("ul");
+    opcoes.forEach((opcao) => {
+        const link = Link(opcao.menu, opcao.rota);
+            link.addEventListener("click", (event) => {
+                event.preventDefault();
+                navega(event.target.getAttribute("href"));
+            })
+        const item = document.createElement("li");
+        item.append(link);
+        lista.append(item);
+    })
+ 
+ const nav = document.createElement("nav");
+ nav.append(lista);
+ return nav;
+
+ }
+
+ /* ferramentas */
+ function navega(rota) {
+    root.innerHTML = "";
+    if (rota === "/login") {
+        PageLogin();
+    } else if (rota === "/home") {
+        PageHome();
+    } else if (rota === "/perfil") {
+        pagePerfil ();
+    } else {
+        root.innerHTML = "<p>Deu ruim</p>";
+    }
+ }
+
+/* paginas */
+function FormLogin() {
+    const email = Input("email", "email", "E-mail");
+    const senha = Input("senha", "password", "Senha");
+    const entrar = Botao("Entrar");
+    entrar.addEventListener("click", (event) => {
+        alert("Clicou!");
+    })
+    const form = document.createElement ("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "/login");
+    form.append(email, senha, entrar);
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        // chamar o backend
+        navega("/home");
+    })
+    return form;
+}
+
 function PageLogin() {
-    const Conteudo = Conteudo();
-    const rodape = Rodape();
+    const logo = Imagem("https://www.svgrepo.com/show/325799/student-graduation-dance.svg", "Logo da Aplicação");
+    const titulo = Titulo("Aluno Online");
+    const form = FormLogin();
+    const link = Link("Esqueceu a Senha?", "/esqueceu-senha");
+    const conteudo = Conteudo();
+    conteudo.append(logo, titulo, form, link);
+    const rodape = Rodape("Copyrigth (C) 2024");
     root.append(conteudo, rodape);
 }
 
 function PageHome() {
     const cabecalho = Cabecalho();
+    const menu = Menu();
     const conteudo = Conteudo();
+    conteudo.append(menu);
+    root.append(cabecalho, conteudo);
+}
+
+function PagePerfil() {
+    const cabecalho = Cabecalho();
+    const menu = Menu();
+    const conteudo = Conteudo();
+    conteudo.append(menu);
     root.append(cabecalho, conteudo);
 }
 
 
-PageLogin();
-
+navega("/login");
