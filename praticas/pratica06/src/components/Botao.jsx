228ab5f9a933454bc5f4039ca012e857b237c3f7
unknown <2424290040@ACADEMICO.IESB.BR>
2024-09-26 21:45:30 -0300

    Subindo a prática06

---
+++ praticas/pratica06/src/components/Botao.jsx
@@ -0,0 +1,8 @@
+function Botao(props) {
+    return (
+        <button onClick={(e) => props.navegaPara ("/home")}>{props.texto}</button>
+        
+    );
+}
+
+export default Botao;
---
+++ praticas/pratica06/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import Conteudo from "../components/Conteudo";
+import Titulo from "../components/Titulo";
+import FormLogin from "./FormLogin";
+
+function Login(props) {
+    return (
+        <Conteudo>
+            <Titulo texto="Aluno Online"/>
+            <FormLogin navegaPara={props.navegaPara} />
+        </Conteudo>
+    );
+}
+export default Login;
